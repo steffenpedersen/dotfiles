28a957598dc913eaac9e7e942ed5abd54d12999b
steffenpedersen <steffen.pedersen@live.dk>
2018-11-04 21:31:56 +0100

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Get dotfiles installation directory
+DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+ln -sf "$DOTFILES_DIR/.gitconfig" ~
+ln -sf "$DOTFILES_DIR/.gitignore_global" ~
+ln -sf "$DOTFILES_DIR/.zshrc" ~
+ln -sf "$DOTFILES_DIR/.zsh_exports" ~
+ln -sf "$DOTFILES_DIR/.zsh_aliases" ~
