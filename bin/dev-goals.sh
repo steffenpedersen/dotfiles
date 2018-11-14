63235f10e1be8507549dcb15bbc3ec46fa261977
steffenpedersen <steffen.pedersen@live.dk>
2018-11-14 20:46:58 +0100

    Add function dev-goals-video in bin

--- bin/dev-goals.sh
+++ bin/dev-goals.sh
@@ -11,3 +11,13 @@ function dev-goals-add() {
       print "Please define a device"
   fi
 }
+
+function dev-goals-video() {
+  if [ $1 = mac ]; then
+      vim $HOME/Dropbox/Web/Git/dev-goals/lists/videos.md
+  elif [ $1 = linux ]; then
+      vim $HOME/Sites/WWW/dev-goals/lists/videos.md
+  else
+      print "Please define a device"
+  fi
+}
