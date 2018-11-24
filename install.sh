504d6c4d0823366d032834588523338ae1f2212f
steffenpedersen <steffen.pedersen@live.dk>
2018-11-24 17:03:37 +0100

    Add npm to install script

--- install.sh
+++ install.sh
@@ -8,3 +8,14 @@ ln -sf "$DOTFILES_DIR/.gitignore_global" ~
 ln -sf "$DOTFILES_DIR/.zshrc" ~
 ln -sf "$DOTFILES_DIR/.zsh_exports" ~
 ln -sf "$DOTFILES_DIR/.zsh_aliases" ~
+
+function npm-install() {
+    npm install -g \
+		terminal-image-cli \
+        uglify-js \
+        uglifycss \
+        webpack \
+        webpack-cli
+}
+
+npm-install
