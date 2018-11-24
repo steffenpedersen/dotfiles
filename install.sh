738e18611c93ad29d0f319e90f429c510edd0b02
steffenpedersen <steffen.pedersen@live.dk>
2018-11-24 17:04:38 +0100

    Add headings to install script

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# Get dotfiles installation directory
+####################
+# SOURCE
+####################
+
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ln -sf "$DOTFILES_DIR/.gitconfig" ~
@@ -8,6 +11,10 @@ ln -sf "$DOTFILES_DIR/.gitignore_global" ~
 ln -sf "$DOTFILES_DIR/.zshrc" ~
 ln -sf "$DOTFILES_DIR/.zsh_exports" ~
 ln -sf "$DOTFILES_DIR/.zsh_aliases" ~
+
+####################
+# NPM
+####################
 
 function npm-install() {
     npm install -g \
