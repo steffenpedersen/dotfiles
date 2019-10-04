3d9bab5c2f3b7f1b21a1c5fcb87ea5e5fd7cd3f1
steffenpedersen <steffen.pedersen@live.dk>
2019-10-04 14:28:59 +0200

    Add the ultimate vimrc

--- install.sh
+++ install.sh
@@ -33,3 +33,13 @@ function npm-install() {
 }
 
 npm-install
+
+####################
+# VIM
+####################
+
+function vimrc-install() {
+    git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+    sh ~/.vim_runtime/install_awesome_vimrc.sh
+}
+
